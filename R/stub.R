f14987aa9087aedbeefbbc4a724b31af58c737d3
Sainath Adapa <adapasainath@gmail.com>
2016-03-20 15:37:09 +0530

    formatting changes

--- R/stub.R
+++ R/stub.R
@@ -3,7 +3,7 @@ compare_args <- function(args1, args2, type = 'exact') {
     if (!setequal(names(args1), names(args2))) return(FALSE)
     return(isTRUE(all.equal(args1, args2[names(args1)])))
   } 
-  
+
   if (type == 'some') {
     intersect_names <- intersect(names(args1), names(args2))
     if (!setequal(names(args1), intersect_names)) return(FALSE)
@@ -37,69 +37,62 @@ strictlyExpectsExternal <- function(..., env_obj) {
 
 withArgsExternal <- function(..., env_obj, type) {
   expected_args <- list(...)
-  
+
   addReturnValue <- function(return_val) {
-    env_obj$return_with_args <- c(env_obj$return_with_args,
-                                  list(list(behavior = 'return',
+    env_obj$return_with_args <- c(list(list(behavior = 'return',
                                             type = type,
                                             return_val = return_val,
-                                            args = expected_args))
-    )
+                                            args = expected_args)),
+                                  env_obj$return_with_args)
     invisible(NULL)
   }
-  
+
   addThrowMsg <- function(msg) {
-    env_obj$return_with_args <- c(env_obj$return_with_args,
-                                  list(list(behavior = 'throw',
+    env_obj$return_with_args <- c(list(list(behavior = 'throw',
                                             return_val = msg,
                                             type = type,
-                                            args = expected_args))
-    )
+                                            args = expected_args)),
+                                  env_obj$return_with_args)
     invisible(NULL)
   }
-  
-  list(returns = addReturnValue,
-       throws = addThrowMsg)
+
+  list(returns = addReturnValue, throws = addThrowMsg)
 }
 
 onCallExternal <- function(num, env_obj) {
-  
+
   addReturnValue <- function(return_val) {
     env_obj$returns_on_call[[as.character(num)]] <- list(behavior = 'return', return_val = return_val)
     invisible(NULL)
   }
-  
+
   addThrowMsg <- function(msg) {
     env_obj$returns_on_call[[as.character(num)]] <- list(behavior = 'throw', return_val = msg)
     invisible(NULL)
   }
-  
+
   strictlyExpects <- function(...) {
     expected_args <- list(...)
-    
+
     env_obj$expectations_on_call[[as.character(num)]] <- list(behavior = 'exact', args = expected_args)
-    
-    invisible(list(
-      returns = addReturnValue,
-      throws = addThrowMsg
-    ))
+
+    invisible(list(returns = addReturnValue,
+                   throws  = addThrowMsg))
   }
-  
+
   expects <- function(...) {
     expected_args <- list(...)
-    
+
     env_obj$expectations_on_call[[as.character(num)]] <- list(behavior = 'some', args = expected_args)
-    
-    invisible(list(
-      returns = addReturnValue,
-      throws = addThrowMsg
-    ))
+
+    invisible(list(returns = addReturnValue,
+                   throws = addThrowMsg))
   }
-  
-  list(returns = addReturnValue,
-       throws = addThrowMsg,
+
+  list(returns         = addReturnValue,
+       throws          = addThrowMsg,
        strictlyExpects = strictlyExpects,
-       expects = expects)
+       expects         = expects)
 }
 
 output_func <- function(behavior, return_val) {
@@ -112,112 +105,111 @@ output_func <- function(behavior, return_val) {
 #' @param function_to_stub is the function that the user wants to make a stub out of
 #' @export
 stub <- function(function_to_stub) {
-  
+
   force(function_to_stub)
-  
+
   data_env <- new.env(hash = FALSE, emptyenv())
-  
-  data_env$stub_called_times <- 0L
-  
+
+  data_env$stub_called_times    <- 0L
+
   data_env$expectations_default <- list()
-  data_env$returns_default <- list()
-  
-  data_env$return_with_args <- list()
-  
+  data_env$returns_default      <- list()
+
+  data_env$return_with_args     <- list()
+
   data_env$expectations_on_call <- list()
-  data_env$returns_on_call <- list()
-  
+  data_env$returns_on_call      <- list()
+
   returnByDefault <- function(return_val) returnByDefaultExternal(return_val, env_obj = data_env)
-  
-  throwByDefault <- function(msg) throwByDefaultExternal(msg, env_obj = data_env)
-  
-  expects <- function(...) expectsExternal(..., env_obj = data_env)
-  
+
+  throwByDefault  <- function(msg) throwByDefaultExternal(msg, env_obj = data_env)
+
+  expects         <- function(...) expectsExternal(..., env_obj = data_env)
+
   strictlyExpects <- function(...) strictlyExpectsExternal(..., env_obj = data_env)
-  
-  withExactArgs <- function(...) withArgsExternal(..., env_obj = data_env, type = 'exact')
-  
-  withArgs <- function(...) withArgsExternal(..., env_obj = data_env, type = 'some')
-  
-  onCall <- function(num) onCallExternal(num, env_obj = data_env)
-  
-  calledTimes <- function() return(data_env$stub_called_times)
-  
+
+  withExactArgs   <- function(...) withArgsExternal(..., env_obj = data_env, type = 'exact')
+
+  withArgs        <- function(...) withArgsExternal(..., env_obj = data_env, type = 'some')
+
+  onCall          <- function(num) onCallExternal(num, env_obj = data_env)
+
+  calledTimes     <- function() return(data_env$stub_called_times)
+
   mock_function <- function(...) {
-    
+
     called_with_args <- as.list(environment(), all = TRUE)
     if ("..." %in% names(called_with_args)) {
       called_with_args['...'] <- NULL
-      called_with_args <- c(called_with_args, list(...))
+      called_with_args        <- c(called_with_args, list(...))
     }
-    called_with_args <- called_with_args[order(names(called_with_args), na.last = TRUE)]
-    
-    stub_called_times_now <- get(x = 'stub_called_times', envir = data_env, inherits = FALSE) + 1L
+
+    stub_called_times_now      <- data_env$stub_called_times + 1L
     data_env$stub_called_times <- stub_called_times_now
     stub_called_times_now_char <- as.character(stub_called_times_now)
-    
+
     if (stub_called_times_now_char %in% names(data_env$expectations_on_call)) {
-      
+
       exp_call_eql <- compare_args(data_env$expectations_on_call[[stub_called_times_now_char]]$args,
                                    called_with_args,
                                    type = data_env$expectations_on_call[[stub_called_times_now_char]]$behavior)
       if (!exp_call_eql) stop(err_msg)
-      
+
     } else if ( length(data_env$expectations_default) > 0L ) {
-      
+
       exp_call_eql <- compare_args(data_env$expectations_default$args,
                                    called_with_args,
                                    type = data_env$expectations_default$behavior)
       if (!exp_call_eql) stop(err_msg)
-      
+
     }
-    
+
     do_this <- list(behavior = 'return', return_val = NULL)
-    
+
     return_behavior_resolved <- FALSE
-    
+
     if (stub_called_times_now_char %in% names(data_env$returns_on_call)) {
-      do_this$behavior <- data_env$returns_on_call[[stub_called_times_now_char]]$behavior
-      do_this$return_val <- data_env$returns_on_call[[stub_called_times_now_char]]$return_val
+      do_this$behavior         <- data_env$returns_on_call[[stub_called_times_now_char]]$behavior
+      do_this$return_val       <- data_env$returns_on_call[[stub_called_times_now_char]]$return_val
       return_behavior_resolved <- TRUE
     }
-    
+
     if ( !return_behavior_resolved && length(data_env$return_with_args) > 0L ) {
       for (this_one in data_env$return_with_args) {
         exp_call_eql <- compare_args(this_one$args, called_with_args, type = this_one$type)
         if (exp_call_eql) {
-          do_this$behavior <- this_one$behavior
-          do_this$return_val <- this_one$return_val
+          do_this$behavior         <- this_one$behavior
+          do_this$return_val       <- this_one$return_val
           return_behavior_resolved <- TRUE
           break
         }
       }
     }
-    
+
     if ( !return_behavior_resolved && length(data_env$returns_default) > 0L ) {
-      do_this$behavior <- data_env$returns_default$behavior
-      do_this$return_val <- data_env$returns_default$return_val
+      do_this$behavior         <- data_env$returns_default$behavior
+      do_this$return_val       <- data_env$returns_default$return_val
       return_behavior_resolved <- TRUE
     }
-    
+
     output_func(do_this$behavior, do_this$return_val)
-    
+
   }
-  
+
   formals(mock_function) <- formals(function_to_stub)
-  
-  list(returns = returnByDefault,
-       throws = throwByDefault,
-       
-       expects = expects,
+
+  list(returns         = returnByDefault,
+       throws          = throwByDefault,
+
+       expects         = expects,
        strictlyExpects = strictlyExpects,
-       
-       withExactArgs = withExactArgs,
-       withArgs = withArgs,
-       
-       onCall = onCall,
-       
-       calledTimes = calledTimes,
-       
-       f = mock_function)
+
+       withExactArgs   = withExactArgs,
+       withArgs        = withArgs,
+
+       onCall          = onCall,
+
+       calledTimes     = calledTimes,
+
+       f               = mock_function)
 }
